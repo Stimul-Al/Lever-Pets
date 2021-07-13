653039e8eda1948d0ae3635871ca71481496ca9a
Stimul-Al <Fktrctqf,fitd1>
2021-07-13 12:35:52 +0300

    add xsuaa and security

---
+++ src/test/java/by/leverx/pets/PetsApplicationTest.java
@@ -0,0 +1,19 @@
+package by.leverx.pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ *
+ * @author aliaksei.babashau
+ */
+@SpringBootTest(classes = PetsApplication.class)
+@java.lang.SuppressWarnings("squid:S2699")
+public class PetsApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
+
